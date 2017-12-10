e41a73666c7854a60086db5d769bcaf0bd6c7e80
Ken <32885973+ykzheng2@users.noreply.github.com>
2017-12-10 18:00:25 -0500

    Add files via upload

---
+++ StudyBU/app/src/main/java/com/example/bustudyspots/ID.java
@@ -0,0 +1,71 @@
+package com.example.bustudyspots;
+
+/**
+ * Created by Daniel Work on 04/12/2017.
+ * each location gets a number, which is passed from the map activity to the scrolling activity.
+ * this minimises the data being passed and allows for the scrolling activity to take care of the
+ * details for each location.
+ * 0 - 49 Bu spaces
+ * 50 - 100 Libraries
+ * 100 - 150 Cafes
+ */
+
+public class ID {
+    //BU spaces
+    public static final int INGALLS = 0;
+    public static final int STUVI2 = 2;
+    public static final int GSU = 3;
+    public static final int PHO  = 4;
+    public static final int SCI  = 5;
+    public static final int LAW  = 6;
+    public static final int CAS  = 7;
+    public static final int YAWKEY  = 8;
+    public static final int HOJO  = 9;
+    public static final int SED = 10;
+    public static final int SCIENGLIB = 11; // Science and Engineering Library
+    public static final int STH = 12; // School of Theology
+    public static final int SAR =13;
+    public static final int STUVI1 = 14;
+    public static final int CFA = 15;
+    public static final int KHC = 16;
+    public static final int COM = 17;
+    public static final int WARREN = 18;
+    public static final int ELIE = 19; // Elie Wiesel Centre for Jewish Studies
+    public static final int CLAFIN = 20;
+    public static final int SLEEPER = 21;
+    public static final int RICH = 22;
+    public static final int TOWERS = 23;
+    public static final int CGS  = 24;
+    public static final int QST = 25;
+    public static final int BUSWELLFOUREIGHT = 26; // 48 Buswell st
+    public static final int ELLIOT = 27; //Elliot H Cole Academic Support Centre
+    public static final int COMMTENNINETEEN = 28; //1019 comm ave
+    public static final int BEACH = 29; //BU BEACH
+    public static final int HILLEL = 30;
+    public static final int SOUTH  = 31; //south campus
+    public static final int FITREC = 32;
+    public static final int ENGL = 33; //Department of English
+    public static final int BARNES = 34;
+    public static final int PSYCH = 35;
+    public static final int DANIELSEN = 36;
+
+    // Libraries
+    public static final int PARDEE = 50;
+    public static final int MUGAR = 51;
+    public static final int ASTRO = 52; //Astronomy Library
+
+    // Cafes
+    public static final int PAVEMENT = 100;
+    public static final int NERO = 101;
+    public static final int SWARREN = 102; // S is for starbucks
+    public static final int SWEST = 103;
+    public static final int SQUEST = 104;
+    public static final int SKENMORE = 105;
+    public static final int TATTE = 106;
+    public static final int LIMERED = 107;
+    public static final int BUICKST = 108;
+    public static final int TBAAR  = 109;
+    public static final int PANERA = 110;
+    public static final int BLUESTATE = 111;
+    public static final int TRIDENT = 112;
+}
